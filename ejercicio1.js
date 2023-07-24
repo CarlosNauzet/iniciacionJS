4aa3d17a3b7f51b9c8ca1b5a3aa92c67c5651787
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-07-24 16:29:40 +0200

    feaT: 1st try ex. 1

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+const boptcampinfo = {
+  nombre: "Nombre del Bootcamp",
+  apellidos: "Tus apellidos",
+  temasConocidos: ["Tema 1", "Tema 2", "Tema 3"], // Agrega los temas que ya conoces
+  enBusquedasActiva: true, // Cambia a false si no estás en búsquedas
+  redesSociales: [
+    {
+      nombreRedSocial: "GitHub",
+      link: "https://github.com/tu_usuario", // cambia esta URL por la de tu perfil de GitHub
+    },
+    // Puedes agregar más redes sociales aquí si lo deseas
+    // Ejemplo:
+    // {
+    //   nombreRedSocial: "LinkedIn",
+    //   link: "https://www.linkedin.com/in/tu_usuario",
+    // },
+  ],
+};
