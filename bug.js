9d6d16bbf1990dcc2b41443acffb77fc13a0bf72
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-07-26 15:44:17 +0200

    feat: ejercicio 2 bug (function not const line 1)

---
+++ bug.js
@@ -0,0 +1,15 @@
+function calcularPromedio(numeros) {
+  console.log(numeros);
+  let sumaTotal = 0;
+  for (let i = 0; i <= numeros.length; i++) {
+    sumaTotal += numeros[i];
+    console.log(sumaTotal);
+  }
+  const promedio = sumaTotal / numeros.length;
+  console.log(promedio + "es este");
+  return promedio;
+}
+const listaNumeros = [1, 2, 3, 4, 5];
+const promedioNumeros = calcularPromedio(listaNumeros);
+
+console.log(listaNumeros);
